099bfc2c36ed20de35ad74de778bea7c2d206c17
Alborz Heydaryan <alborz@crowdbotics.com>
2023-10-05 12:41:31 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "alborz-test-5-dev-119124",
   url: "https://alborz-test-5-dev-119124.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
